5effac8da504a0732f5b1fb416534df0162dc9a1
EJ Josephat <josephat.1@osu.edu>
2020-09-22 09:11:41 -0400

    pivacy

---
+++ legal/privacy.php
@@ -0,0 +1,3 @@
+<h1>Privacy</h1>
+
+<p>We do not collect any information except for what makes the service work (refer to /legal/terms.php for more information). Third party services we utilize to make this work will collect information though. You have been warned.</p>
---
+++ legal/terms.php
@@ -0,0 +1,7 @@
+<?php
+
+<h1>Terms of Service</h1>
+
+<p>This service is free and provided "as is" with absolutely warranty. You may <u>absolutely</u> not sue us or launch a legal complaint under any circumstance.</p>
+<p>Using this service implies and means that agree with whatever terms we set forward at any point, that are applicable retroactively.</p>
+
